dc26a5feddf41f4026680cde8fc21dc435c31099
Domenico Pettinato <mimmo.pettinato@gmail.com>
2024-09-17 21:17:11 +0200

    laravel fortify completa

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,15 @@
+
+
+
+<!-- validazione del login -->
+<div class="col-12  mt-3 d-flex justify-content-center">
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+            </div>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,13 @@
+
+
+<!-- validazione della registrazione -->
+<div class="col-12  mt-3 d-flex justify-content-center">
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
