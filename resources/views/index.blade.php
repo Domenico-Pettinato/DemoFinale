ae0633569f6c6688e9d916f87f08b920e4876537
FedericoMassidda <chiccomassidda@gmail.com>
2024-09-18 16:38:39 +0200

    aggiornamento grafico

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <title>Document</title>
-</head>
-<body>
-    
-    <h1>ciao</h1>
-    <button type="button" class="btn btn-success rounded-pill">Inserisci Annuncio</button>
-</body>
-</html>
+<x-layout>
+    <x-navbar/>
+    <div class="container mt-4">
+        <h1>ciao</h1>
+        <button type="button" class="btn btn-success rounded-pill">Inserisci Annuncio</button>
+    </div>
+</x-layout>
