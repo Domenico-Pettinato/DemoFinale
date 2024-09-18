4f8df6575cdbb9a3d2997376c9b3bf593de921c2
TaniaPrete <166862270+TaniaPrete@users.noreply.github.com>
2024-09-18 17:12:53 +0200

    merge

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,7 +1,7 @@
 <div class="card" style="width: 18rem;">
     <img src="..." class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">{{title}}</h5>
+      <h5 class="card-title">{{$title}}</h5>
       <p class="card-text">Assassin’s Creed Mirage è un videogioco di azione e avventura sviluppato da Ubisoft Bordeaux e pubblicato da Ubisoft. È il tredicesimo capitolo principale della serie Assassin’s Creed e si presenta come un ritorno alle origini della saga, con un’enfasi maggiore sulla narrazione lineare e sul gameplay stealth rispetto ai capitoli più recenti12.
 
         La storia è ambientata nella Baghdad del IX secolo durante l’epoca d’oro islamica e segue le avventure di Basim Ibn Ishaq, un giovane ladruncolo di strada perseguitato da visioni spaventose. Basim cerca risposte e giustizia, e il suo viaggio lo porta a unirsi alla Confraternita degli Assassini12. La trama esplora la sua transizione da ladro a membro a pieno titolo della Confraternita, che lotta per la pace e la libertà contro l’Ordine dei Templari12.
