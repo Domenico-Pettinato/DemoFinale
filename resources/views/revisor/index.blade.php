c9ee86b7c1832b2a2bc420832dcec9c61288451e
FedericoMassidda <chiccomassidda@gmail.com>
2024-09-23 21:04:04 +0200

    iniziata la logica per accettare e rifiutare l'annuncio

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -1,6 +1,26 @@
 <x-layout>
     <h1 class="text-center">Benvenuto nella tua area riservata</h1>
-
+    <div class="container">
+        <div class="row justify-content-center">
+            @if ($article_to_check)
+                <x-card :article="$article_to_check"/> 
+                <div class="d-flex justify-content-center">
+                    <form action="" method="POST" class="mt-4 mb-5 me-5">
+                        @csrf
+                        <button type="button" class="btn btn-success btnRevisorCustom">Accetta</button>
+                    </form>
+                    <form action="" method="POST" class="mt-4 mb-5">
+                        @csrf
+                        <button type="button" class="btn btn-danger btnRevisorCustom">Rifiuta</button>
+                    </form>
+                </div>
+            @else
+                <div class="col-12">
+                    <h2 class="text-center">Nessun articolo da revisionare</h2>
+                </div>
+            @endif
+        </div>
+    </div>
 </x-layout>
 
 
