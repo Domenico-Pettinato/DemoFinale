7dbb36dfa39680f915743f8cc52f958c296f0562
Domenico Pettinato <mimmo.pettinato@gmail.com>
2024-09-25 20:48:35 +0200

    mailtrap funge

--- app/Http/Controllers/WorkWithUsController.php
+++ app/Http/Controllers/WorkWithUsController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\CandidaturaRicevuta;
+use App\Mail\InfoMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use PharIo\Manifest\Email;
 
 class WorkWithUsController extends Controller
 {
@@ -12,8 +16,10 @@ class WorkWithUsController extends Controller
         return view('workwithus');
     }
 
-    public function submitApplication(Request $request)
+    public function submitapplication(Request $request)
     {
+
+        //dd($request->all());
         // Validazione del form
         $request->validate([
             'name' => 'required|string|max:255',
@@ -21,6 +27,17 @@ class WorkWithUsController extends Controller
             'cv' => 'required|file|mimes:pdf,doc,docx|max:2048', // validazione del CV
         ]);
 
+
+        // Dettagli dell'email
+        $contact = [
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+        ];
+
+        // Invia l'email
+        $contact = $request->all();
+        Mail::to('mimmo.pettinato@gmail.com')->send(new InfoMail($contact));
+       
         session()->flash('success', 'Candidatura inviata con successo!');
         return redirect()->route('workwithus');
     }
---
+++ resources/views/email.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Candidatura Ricevuta</h1>
+    <p>{{ $contact['name'] }},grazie per aver inviato la tua candidatura con la mail {{ $contact['email'] }}</p>
+</body>
+</html>
+    
+
