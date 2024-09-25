1c6b78b9f70681a5eddc00df7900fa38773de95c
FedericoMassidda <chiccomassidda@gmail.com>
2024-09-25 20:57:38 +0200

    aggiunta real time validation con error messages custom

--- app/Livewire/CreateArticle.php
+++ app/Livewire/CreateArticle.php
@@ -5,17 +5,42 @@ namespace App\Livewire;
 use App\Models\Article;
 use App\Models\Category;
 use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class CreateArticle extends Component
 {
+    #[Validate()]
     public $title;
+    #[Validate()]
     public $price;
+    #[Validate()]
     public $description;
+    #[Validate()]
     public $category_id;
 
+    // validation
+    public function rules(){
+        return [
+            'title'=> 'required',
+            'price'=> 'required',
+            'description'=> 'required|min:10|max:1000',
+            'category_id'=> 'required',
+        ];
+    }
+    // messages
+    protected $messages = [
+        'title.required' => 'Il titolo è obbligatorio.',
+        'price.required' => 'Il prezzo è obbligatorio.',
+        'description.required' => 'La descrizione è obbligatoria.',
+        'description.min' => 'La descrizione deve avere almeno 10 caratteri.',
+        'description.max' => 'La descrizione deve avere massimo 1000 caratteri.',
+        'category_id.required' => 'La categoria è obbligatoria.'
+    ];
     // store articles
     public function store(){
+        $this->validate();
+        
         Article::create([
             'title'=>$this->title,
             'price'=>$this->price,
--- resources/views/livewire/create-article.blade.php
+++ resources/views/livewire/create-article.blade.php
@@ -7,21 +7,33 @@
         <form wire:submit="store">
             <div class="mb-3">
                 <label for="createArticleTitle" class="form-label">Titolo</label>
-                <input type="text" class="form-control" id="createArticleTitle" aria-describedby="emailHelp" wire:model="title">
+                <input type="text" class="form-control" id="createArticleTitle" aria-describedby="emailHelp" wire:model.blur="title">
+                @error('title')
+                    <p class="text-danger fst-italic">{{$message}}</p>
+                @enderror
             </div>
-            <select class="form-select" aria-label="Default select example" wire:model="category_id">
+            <select class="form-select" aria-label="Default select example" wire:model.blur="category_id">
                 <option value="" selected>Scegli la tua categoria</option>
                 @foreach ($categories as $category)
                     <option value="{{$category->id}}">{{$category->name}}</option>
                 @endforeach
             </select>
+            @error('category_id')
+                <p class="text-danger fst-italic">{{$message}}</p>
+            @enderror
             <div class="mb-3">
                 <label for="createArticlePrice" class="form-label">Prezzo</label>
-                <input type="text" class="form-control" id="createArticlePrice" wire:model="price">
+                <input type="text" class="form-control" id="createArticlePrice" wire:model.blur="price">
+                @error('price')
+                    <p class="text-danger fst-italic">{{$message}}</p>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="createArticleDescription" class="form-label">Descrizione</label>
-                <textarea class="form-control" id="createArticleDescription" rows="3" wire:model="description"></textarea>
+                <textarea class="form-control" id="createArticleDescription" rows="3" wire:model.blur="description"></textarea>
+                @error('description')
+                    <p class="text-danger fst-italic">{{$message}}</p>
+                @enderror
             </div>
             <button type="submit" class="btn btn-primary">Carica annuncio</button>
         </form>
