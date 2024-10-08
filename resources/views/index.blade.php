e483b521e0d44bcf71a6a5d407c9514eebf0b64f
FedericoMassidda <chiccomassidda@gmail.com>
2024-10-08 18:48:16 +0200

    cambiata totalmente la index del sito

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,24 +1,21 @@
 <x-layout>
-    <x-sidebar/>
+    {{-- normale --}}
+    {{-- <x-sidebar/>
     <div class="container">
         <div class="row">
-             <!-- @foreach ($articles as $article)
-                <x-card :article="$article"/> 
-            @endforeach -->
-             @foreach ($article->images as $key => $image)
-            <div class="col-6 col-md-4 mb-4 text-center">
-                <img src="{{ $image->getUrl(300, 300) }}" class="img-fluid rounded shadow" alt="Immagine {{$key + 1 }} dell'articolo {{ $article_to_chack->title }}">
-            </div>
-            @endforeach
             @foreach ($articles as $article)
                 <x-card :article="$article"/> 
             @endforeach
-            {{-- @foreach ($article->images as $key => $image)
-            <div class="col-6 col-md-4 mb-4 text-center">
-                <img src="{{ $image->getUrl(300, 300) }}" class="img-fluid rounded shadow" alt="Immagine {{$key + 1 }} dell'articolo {{ $article_to_chack->title }}">
-            </div>
-            @endforeach --}}
         </div>
-        {{-- <div style="height: 2000vh"></div> --}}
+    </div> --}}
+    {{-- carosello --}}
+    <div id="carouselExample" class="carousel slide carouselCustom" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            @foreach ($articles as $article)
+                <div class="carousel-item {{$loop->first ? 'active' : ''}}">
+                    <x-indexCardCarousel :article="$article"/>
+                </div>
+            @endforeach
+        </div>
     </div>
 </x-layout>
